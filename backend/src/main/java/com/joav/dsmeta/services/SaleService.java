277f72bc5731dcac851c183323149e3d23941fb5
joav2 <joaovitork135@gmail.com>
2022-07-27 04:35:45 -0300

    API test

---
+++ backend/src/main/java/com/joav/dsmeta/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.joav.dsmeta.services;
+
+import com.joav.dsmeta.entities.Sale;
+import com.joav.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale> findSales() {
+        return repository.findAll();
+    }
+}
