a09d22d4c7e75bc6e08e61fff2afb72c35b02da5
joav2 <joaovitork135@gmail.com>
2022-07-27 02:19:50 -0300

    Project created

---
+++ backend/src/test/java/com/joav/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joav.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
